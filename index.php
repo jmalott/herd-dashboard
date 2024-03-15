1a88d9b15d2acc3c5adc44044d77834cb71dd095
Josh Wright <144492511+jwrightmm@users.noreply.github.com>
2024-03-15 09:48:27 -0400

    Update index.php
    Different styling, hide the other lists and just have one centered list

--- index.php
+++ index.php
@@ -9,18 +9,34 @@ $tld = isset($valet_config->tld) ? $valet_config->tld : $valet_config->domain;
     <title>Valet Dashboard</title>
     <head>
         <script src="https://cdn.tailwindcss.com"></script>
+        <style>
+            li {
+                margin-bottom:10px;
+            }
+            .site-list {
+                display:none;
+            }
+            .site-list.list-2 {
+                display:block;
+            }
+        </style>
     </head>
     <body class="m-8 font-sans">
         <div class="grid sm:grid-cols-[repeat(auto-fit,minmax(280px,1fr))] sm:justify-items-center">
+            <?php $i = 0; ?>
             <?php foreach ($valet_config->paths as $parked_path) : ?>
-                <div class="leading-normal whitespace-no-wrap m-2">
+                <?php $i++; ?>
+                <div class="leading-normal whitespace-no-wrap m-2 site-list list-<?php echo $i; ?>">
                     <code class="font-mono text-gray-600"><?= str_replace(getenv('HOME'), '~', $parked_path) ?></code>
-                    <ul class="list-disc pl-4">
+                    <ul>
                         <?php foreach (scandir($parked_path) as $site) : ?>
                             <?php if ($site == basename(__DIR__)): continue; endif ?>
                             <?php if ((is_dir("$parked_path/$site") || is_link("$parked_path/$site")) && $site[0] != '.') : ?>
                             <li><a href="http://<?= "$site.$tld" ?>/" target="<?= "valet_$site" ?>"
-                                class="text-blue-500 hover:text-blue-400 no-underline hover:underline"><?= "$site.$tld" ?></a></li>
+                                class="relative inline-block px-4 py-2 font-medium group">
+                                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                                <span class="relative text-black group-hover:text-white"><?= "$site.$tld" ?></span></a></li>
                             <?php endif ?>
                         <?php endforeach ?>
                     </ul>
